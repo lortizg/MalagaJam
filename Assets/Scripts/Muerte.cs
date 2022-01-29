68d24fffe3c6c7e6491d30991d8175587031b8c2
Laura <laura.og179@gmail.com>
2022-01-29 13:14:42 +0100

    mundo paralelo no colision

---
+++ Assets/Scripts/Muerte.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muerte : MonoBehaviour
+{
+    private UnityEngine.GameObject mundoMuerte;
+    private UnityEngine.GameObject mundo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mundoMuerte = GameObject.Find("Muerte");
+        mundo= GameObject.Find("Tilemap");
+
+        //mundoMuerte.transform.position = new Vector3(2*mundo.transform.position.x,mundo.transform.position.y, mundo.transform.position.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D Rigidbody2D;
+    private float horizontal;
+    private bool alreadyJumping=false;
+    private float muertePosition;
+    private bool mundoFalso = false;
+    private GameObject[] objetosAIgnorarFalso;
+    private GameObject[] objetosAIgnorarTrue;
+    public GameObject mundo;
+
+    public float JumpForce=150;
+    public float Speed=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        muertePosition = GameObject.Find("Muerte").transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f)) { alreadyJumping = false; }
+
+        //giramos si vamos a la izquierda
+        if (horizontal < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        else if (horizontal > 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+
+        // Saltamos si pulsamos w
+        if (Input.GetKeyDown(KeyCode.W) && !alreadyJumping)
+        {
+            Jump();
+        }
+
+        // cambiar mapa
+        if (Input.GetKeyDown(KeyCode.F5))
+        {
+            if (!mundoFalso)
+            {
+                cambiarMapa(GameObject.Find("Muerte").transform);
+                mundoFalso = true;
+                // enableamos colision con muerte
+                foreach (GameObject obj in objetosAIgnorarTrue) {
+                    Physics2D.IgnoreCollision(obj.GetComponent<Collider2D>(), GetComponent<Collider2D>(),false);
+                }
+                // disableamos con mundo
+                foreach (GameObject obj in objetosAIgnorarFalso) {
+                    Physics2D.IgnoreCollision(obj.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+                }
+                
+            }
+            else
+            {
+                cambiarMapa(GameObject.Find("Tilemap").transform);
+                mundoFalso = false;
+                // enableamos colision con mundo
+                foreach (GameObject obj in objetosAIgnorarFalso) {
+                    Physics2D.IgnoreCollision(obj.GetComponent<Collider2D>(), GetComponent<Collider2D>(), false);
+                }
+                // disableamos con muerte
+                foreach (GameObject obj in objetosAIgnorarTrue) {
+                    Physics2D.IgnoreCollision(obj.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+                }
+            }
+
+        }
+    }
+    private void OnEnable() {
+        objetosAIgnorarFalso = GameObject.FindGameObjectsWithTag("Mundo");
+        objetosAIgnorarTrue = GameObject.FindGameObjectsWithTag("Muerte");
+    }
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //Debug.Log(mundoFalso);
+        //Debug.Log(collision.gameObject.tag);
+        if((collision.gameObject.tag=="Muerte" && !mundoFalso) || (collision.gameObject.tag == "Mundo" && mundoFalso)) {
+            //Debug.Log("AAAAAAA");
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), collision.collider.GetComponent<Collider2D>());
+        }
+    }
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up*JumpForce);
+        alreadyJumping = true;
+    }
+    private void cambiarMapa(Transform mundo) {
+        //Debug.Log(mundo.transform.position.z);
+        transform.position = new Vector3(transform.position.x, transform.position.y, mundo.transform.position.z);
+    }
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity =new Vector2(horizontal*Speed,Rigidbody2D.velocity.y);
+    }
+
+
+}
